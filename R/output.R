82d0c94a4ee5074ad391d93272e69c6e1743aa39
root <root@chp-eingabe.us-central1-f.c.checkpoint-data-input.internal>
2021-01-02 11:30:37 +0000

    summary vars

--- R/output.R
+++ R/output.R
@@ -1,5 +1,5 @@
 
-test=add_summary(ds1)
-t1=as.data.frame(test)
-library(WriteXLS)
-WriteXLS("t1", "sums.xlsx", row.names = T, AdjWidth = F, BoldHeaderRow = T, FreezeRow = 1, FreezeCol = 1, AllText = T)
+# test=add_summary(ds1)
+# t1=as.data.frame(test)
+# library(WriteXLS)
+# WriteXLS("t1", "sums.xlsx", row.names = T, AdjWidth = F, BoldHeaderRow = T, FreezeRow = 1, FreezeCol = 1, AllText = T)
